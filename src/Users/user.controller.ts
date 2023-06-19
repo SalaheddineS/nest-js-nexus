2081a3a18a2d0803de6aa4ecb19596f1c284c195
Salaheddine <Salaheddinemorchid1@gmail.com>
2023-06-19 11:56:52 +0100

    + Behavior to execute N-tier pattern

--- src/Users/user.controller.ts
+++ src/Users/user.controller.ts
@@ -5,25 +5,18 @@ import { User } from './UserEntity';
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  userList: User[] = [];
+  
 
   @Get('showUsers')
   getAllUsers(): User[] {
-    return this.userList;
+    return this.userService.getAllUsers();
   }
 
   @Post('addUser')
   addUser(
     @Body() userInfo:User
   ): User {
-    const newUser = new User(userInfo.fullName, userInfo.email, userInfo.password, []);
-    try {
-      this.userList.push(newUser);
-      return newUser;
-    } catch (err) {
-      console.error('An error occurred while adding a new user: \n');
-      throw err;
-    }
+    return this.userService.addUser(userInfo);
   }
 
 }
--- src/Users/user.service.ts
+++ src/Users/user.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { User } from './UserEntity';
 
 @Injectable()
-export class UserService{}
+export class UserService {
+  userList: User[] = [];
+
+
+  getAllUsers():User[]{
+    return this.userList
+  }
+
+  addUser(userInfo: User): User {
+    const newUser = new User(
+      userInfo.fullName,
+      userInfo.email,
+      userInfo.password,
+      [],
+    );
+    try {
+      this.userList.push(newUser);
+      return newUser;
+    } catch (err) {
+      console.error('An error occurred while adding a new user: \n');
+      throw err;
+    }
+  }
+}
